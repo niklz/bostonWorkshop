0d37b8ee3bdb96dc068b7a439a5d394969a8b41f
Nick Howlett <nhowlett@Mango.local>
2017-10-27 16:51:01 +0100

    finished script

--- R/textAnalysis.R
+++ R/textAnalysis.R
@@ -48,3 +48,74 @@ tidyWizzardNgram <- wizardOfOz %>% unnest_tokens(word,
 
 tidyWizzardNgram %>% count(word, sort = TRUE)
 
+# exercise
+
+tidyIslandNgram <- treasureIsland %>% unnest_tokens(word,
+                                                    text,
+                                                    token = "ngrams",
+                                                    n = 3)
+tidyIslandNgram %>% count(word, sort = TRUE)
+
+# wordcloud
+library(wordcloud)
+library(tidyr)
+
+cloudWizzard <- wizardOfOz %>%
+                unnest_tokens(word, text) %>%
+                anti_join(stop_words) %>%
+                count(word, sort = TRUE)
+
+wordcloud(cloudWizzard$word, cloudWizzard$n, max.words = 50, colors = "red")
+
+compCloud <- wizardOfOz %>%
+             unnest_tokens(word, text) %>%
+             anti_join(stop_words) %>%
+             inner_join(get_sentiments("bing")) %>%
+             count(word, sentiment, sort = TRUE) %>%
+             spread(sentiment, n, fill = 0) %>%
+             data.frame()
+
+rownames(compCloud) <- compCloud$word
+select(compCloud, -word) %>%
+  comparison.cloud(colors = c("darkred", "darkgreen"),
+                   max.words = 100)
+
+# exercise
+
+compCloudTreas <- treasureIsland %>%
+                  unnest_tokens(word, text) %>%
+                  anti_join(stop_words) %>%
+                  inner_join(get_sentiments("bing")) %>%
+                  count(word, sentiment, sort = TRUE) %>%
+                  spread(sentiment, n, fill = 0) %>%
+                  data.frame()
+
+rownames(compCloudTreas) <- compCloudTreas$word
+select(compCloudTreas, -word) %>%
+  comparison.cloud(colors = c("darkred", "darkgreen"),
+                   max.words = 100)
+
+
+library(igraph)
+library(ggraph)
+
+tidyWizzardNgram <- wizardOfOz %>% 
+                    unnest_tokens(word,
+                                  text,
+                                  token = "ngrams",
+                                  n = 2) %>%
+                    count(word, sort = TRUE) %>%
+                    separate(word, c("firstWord", "secondWord", sep = " ")) %>%
+                    anti_join(stop_words, by = c("firstWord" = "word")) %>%
+                    anti_join(stop_words, by = c("secondWord" = "word")) %>%
+                    filter(firstWord == "woodman")
+
+iGraphObject <- tidyWizzardNgram %>%
+                graph_from_data_frame()
+
+#plotting
+
+ggraph(iGraphObject) +
+geom_node_text(aes(label = name), vjust = 1, hjust = 1) +
+geom_node_point() +
+geom_edge_link()
